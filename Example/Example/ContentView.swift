d2a3b4828525520df23ed5fd2beeaed644171055
tisfeng <tisfeng@gmail.com>
2024-12-02 21:50:09 +0800

    fix: update project, remove unused code

---
+++ Example/Example/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Example
+//
+//  Created by tisfeng on 2024/12/2.
+//
+
+import SwiftUI
+import AcknowKit
+
+struct ContentView: View {
+
+    var body: some View {
+        NavigationStack {
+            AcknowLibraryForm()
+        }
+    }
+}
+
+
+#Preview {
+    ContentView()
+}
---
+++ Example/Example/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleApp.swift
+//  Example
+//
+//  Created by tisfeng on 2024/12/2.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
